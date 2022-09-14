181c9583dc719cfd747c6288cf3906efbe64a0c6
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-09-14 13:06:51 +0300

    fix cors for dev access

--- back-end/index.js
+++ back-end/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 2501;
 
 const app = express();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
 const corsOptions = {
   origin: allowedOrigins,
   credentials: true,
