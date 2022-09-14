a692d12b5713bcfbb4c7b695543d6a3bdb986334
Yossi Rise <10915495-yossirise@users.noreply.gitlab.com>
2022-09-14 19:55:12 +0300

    Add not-yet-working endpoint to update user profile

---
+++ back-end/middleware/verifyAdminOrParamIsUser.js
@@ -0,0 +1,16 @@
+const { notify } = require("../routes/signup");
+
+module.exports = function (req, res, next) {
+  const { user, params } = req;
+
+  try {
+    if (user._id !== params.id && !user.isAdmin) {
+      throw { status: 403, message: "access denied" };
+    }
+
+    next();
+  } catch (err) {
+    err.status ??= 400;
+    next(err);
+  }
+};
