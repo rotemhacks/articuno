0c02e1bd3fb2dec27086fff03a42a903eeda8524
Rotem S <65527552+planetariumfish@users.noreply.github.com>
2022-09-14 12:54:48 +0300

    implement logout function

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -1,30 +1,49 @@
-import { Avatar, Box, Heading, SimpleGrid, VStack } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Heading,
+  SimpleGrid,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import React from "react";
 import { ActiveUser } from "../contexts/contexts";
 import axios from "../utils/axiosClient";
 
 const Profile = () => {
-  const { user } = React.useContext(ActiveUser);
+  const { user, setUser } = React.useContext(ActiveUser);
+
+  const handleLogout = async () => {
+    const response = await axios.post("/logout", user.firstname);
+    setUser(null);
+  };
 
   return (
-    <Box>
-      <Heading>Profile</Heading>
+    <VStack justify="center">
+      <Heading mb={3}>Profile</Heading>
       {user && (
-        <SimpleGrid
-          columns={[1, null, 2]}
-          spacing={["2rem", null, "1rem"]}
-          width={["90%", null, "60%"]}
-        >
-          <Avatar
-            src={user.picture}
-            name={`${user.firstname} ${user.lastname}`}
-          />
-          <VStack>
-            {/* Name, email, bio, showing NSFW, subscribed tags, blacklisted tags */}
-          </VStack>
-        </SimpleGrid>
+        <>
+          <SimpleGrid
+            columns={[1, null, 2]}
+            spacing={["2rem", null, "1rem"]}
+            // width={["90%", null, "60%"]}
+          >
+            <Avatar
+              src={user.picture}
+              name={`${user.firstname} ${user.lastname || ""}`}
+              bg="brand.Aero"
+              size="2xl"
+            />
+            <VStack>
+              <Text>{`${user.firstname} ${user.lastname || ""}`}</Text>
+              {/* Name, email, bio, showing NSFW, subscribed tags, blacklisted tags */}
+            </VStack>
+          </SimpleGrid>
+          <Button onClick={handleLogout}>Logout</Button>
+        </>
       )}
-    </Box>
+    </VStack>
   );
 };
 
